52ca65b35b960f5c245ead5edc7c7cd30070b163
Prithiv <theprithivrajs@gmail.com>
2025-02-22 13:12:47 +0530

    Update railway.nix

--- railway.nix
+++ railway.nix
@@ -1,7 +1,15 @@
 { pkgs }: {
   deps = [
+    pkgs.stdenv.cc.cc.lib  # Essential C libraries
     pkgs.libglvnd  # Provides libGL.so.1
+    pkgs.libglvnd.dev
     pkgs.glib
     pkgs.zlib
+    pkgs.freetype  # Font rendering support for OpenCV
+    pkgs.xorg.libX11  # X11 libraries required by OpenCV
+    pkgs.xorg.libXext
+    pkgs.xorg.libXrender
+    pkgs.xorg.libXtst
+    pkgs.xorg.libXfixes
   ];
 }
